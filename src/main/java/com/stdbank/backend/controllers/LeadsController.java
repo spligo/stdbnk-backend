ef6bf0825aaad7a78ece31172cdb14cf9fa0d1bd
Tebogo Ntehelang <tth.ntehelang@gmail.com>
2022-04-10 17:03:38 +0200

    added logger

--- src/main/java/com/stdbank/backend/controllers/LeadsController.java
+++ src/main/java/com/stdbank/backend/controllers/LeadsController.java
@@ -1,5 +1,8 @@
 package com.stdbank.backend.controllers;
 
+import com.stdbank.backend.services.CsvImportService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -9,10 +12,13 @@ import java.util.Map;
 @RequestMapping("/leads")
 public class LeadsController {
 
+    private static final Logger logger = LoggerFactory.getLogger(LeadsController.class);
+
     @CrossOrigin
     @PostMapping
     public ResponseEntity<Boolean> createLead(@RequestBody Map<String, Object> lead) {
 
+        logger.info("Saving lead {}", lead);
         return ResponseEntity.ok(true);
 
     }
